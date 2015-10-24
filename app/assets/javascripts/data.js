3ae133bd6fa1f5e63215e9e59ad40a0df54f3947
eduardopoleo <eduardopoleo@gmail.com>
2015-10-24 15:33:33 -0400

    Properly indented

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,5 +1,5 @@
 $(function() {
-  url = 'all_salaries'
+  url = 'administrative_staff'
   ajax_call(url)
 
   function ajax_call(url) {
@@ -30,84 +30,81 @@ $(function() {
     var yMargin = 0
 
     var rectMargin = 5
-    // d3.select(".chart")
-    //       .attr("width", width + margin.left + margin.right)
-    //       .attr("height", height)
-    //    .append("g")
-    //       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
     svg = d3.select('body')
-              .append('svg')
-              .attr("width", w)
-              .attr("height", h)
-              .append("g")
-              .attr("transform", "translate(" + xMargin + "," + yMargin + ")");
+                  .append('svg')
+                  .attr("width", w)
+                  .attr("height", h)
+                  .append("g")
+                  .attr("transform", "translate(" + xMargin + "," + yMargin + ")");
 
+    // In order for the left axis to show I need to translate the chart enough
     var xScale = d3.scale.linear()
-                         .domain([0, d3.max(dataSet, function (d) {
-                           return d.average_salary
-                         })])
-                         .range([xPadding, w - xMargin])
+                            .domain([0, d3.max(dataSet, function (d) {
+                             return d.average_salary
+                            })])
+                            .range([xPadding, w - xMargin])
 
     var yScale = d3.scale.ordinal()
-                          .domain(d3.range(dataSet.length))
-                          .rangeRoundBands([0, (h - 1.2 * yPadding )], 0.05)
-
-    var yAxisScale = d3.scale.ordinal()
-                            .domain(dataSet.map(function (d) {
-                              return d.university
-                            }))
+                            .domain(d3.range(dataSet.length))
                             .rangeRoundBands([0, (h - 1.2 * yPadding )], 0.05)
 
+    var yAxisScale = d3.scale.ordinal()
+                                .domain(dataSet.map(function (d) {
+                                  return d.university
+                                }))
+                                .rangeRoundBands([0, (h - 1.2 * yPadding )], 0.05)
+
     rects = svg.selectAll('rect')
-               .data(dataSet)
-               .enter()
-               .append("rect")
-               .attr("x", xPadding)
-               .attr("y", function (d, i) {
-                 return yScale(i)
-               })
-               .attr("width", function (d) {
-                 return xScale(d.average_salary)
-               })
-               .attr("height", function () {
-                 return h/dataSet.length - rectMargin
-               })
+                    .data(dataSet)
+                    .enter()
+                    .append("rect")
+                    .attr("x", xPadding)
+                    .attr("y", function (d, i) {
+                    return yScale(i)
+                    })
+                    .attr("width", function (d) {
+                    return xScale(d.average_salary)
+                    })
+                    .attr("height", function () {
+                    return h/dataSet.length - rectMargin
+                    })
 
     svg.selectAll("text")
-              .data(dataSet)
-              .enter()
-              .append("text")
-              .text(function (d) {
-                return d.average_salary
-              })
-              .attr("x", function (d) {
-                return xScale(d.average_salary) - 40 //magic
-              })
-              .attr("text-anchor", "middle")
-              .attr("y", function (d, i) {
-                return yScale(i) + yScale.rangeBand() / 1.7;//magic
-              })
-              .attr("font-family", "sans-serif")
-              .attr("font-size", "11px")
-              .attr("fill", "red");
+            .data(dataSet)
+            .enter()
+            .append("text")
+            .text(function (d) {
+            return d.average_salary
+            })
+            .attr("x", function (d) {
+            return xScale(d.average_salary) - 40 //magic
+            })
+            .attr("text-anchor", "middle")
+            .attr("y", function (d, i) {
+            return yScale(i) + yScale.rangeBand() / 1.7;//magic
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px")
+            .attr("fill", "red");
 
     var xAxis = d3.svg.axis()
-                      .scale(xScale)
-                      .orient("bottom")
-                      .ticks(10)
+                  .scale(xScale)
+                  .orient("bottom")
+                  .ticks(5)
 
     var yAxis = d3.svg.axis()
-                      .scale(yAxisScale)
-                      .orient("left")
+                  .scale(yAxisScale)
+                  .orient("left")
 
     svg.append("g")
-       .attr("class", "axis")
-       .attr("transform", "translate(0," + (h - yPadding) + ")")
-       .call(xAxis);
+          .attr("class", "axis")
+          .attr("transform", "translate(0," + (h - yPadding) + ")")
+          .call(xAxis);
 
     svg.append("g")
-      .attr("class", "axis")
-      .call(yAxis);
-  }
+          .attr("class", "axis")
+          .call(yAxis);
+          }
 });
