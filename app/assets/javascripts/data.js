278df477d1cbe1a5534fe843500f96cd84f16f48
eduardopoleo <eduardopoleo@gmail.com>
2015-10-23 20:51:56 -0400

    Rebuilding the plot

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,11 +1,6 @@
 $(function() {
-  ajax_call('all_salaries')
-
-  $('.choice').click(function(e) {
-    var url = e.target.defaultValue
-    $(".chart").empty()
-    ajax_call(url)
-  });
+  url = 'all_salaries'
+  ajax_call(url)
 
   function ajax_call(url) {
     $.ajax({
@@ -14,69 +9,64 @@ $(function() {
      url: url,
      dataType: 'json',
      success: function (data) {
-       console.log(data)
-       draw(data["averages"]);
+       console.log(data["averages"])
      },
      error: function (result) {
          error();
      }
     });
   }
-  //wrap this on a onchange for all the radio buttons with the same class
-  //When a button with that class gets tickle it passes the info down,(maybe just and index and gets parsed in here)
-  function draw(data) {
-    var that = this;
-    var margin = {top: 40, right: 50, bottom: 40, left: 200},
-    width = 960 - margin.left - margin.right,
-    barHeight = 40,
-    height = barHeight * data.length + margin.top + margin.bottom;
 
-    var x = d3.scale.linear()
-        .range([0, width])
-        .domain([0, 245852.56]);
-        //This magic number is the max average_salary among all my data sets
-        // I do this so that the bars make sense agains each other.
+  dataSet = [
+    {
+      university: "Algoma",
+      average: 3
+    },
 
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
+    {
+      university: "Queens",
+      average: 5
+    },
+    {
+      university: "Queens",
+      average: 8
+    }
+  ]
+  draw(dataSet)
 
-    var y = d3.scale.ordinal()
-        .domain(data.map(function(d) {return d.university; }))
-        .rangeRoundBands([10  , height - 100], -1);
+  function draw(dataSet) {
+    w = 100
+    h = 200
+    rectMargin = 5
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
 
-    var chart = d3.select(".chart")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var bar = chart.selectAll("g")
-        .data(data)
-      .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    svg = d3.select('body')
+            .append('svg')
+            .attr("width", w)
+            .attr("height", h)
 
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.average_salary); })
-        .attr("height", barHeight - 8);
+    var xScale = d3.scale.linear()
+                         .domain([0, d3.max(dataSet, function (d) {
+                           return d.average
+                         })])
+                         .range([0, w])
 
-    bar.append("text")
-        .attr("x", function(d) { return x(d.average_salary) - 3; })
-        .attr("y", barHeight / 2.5)
-        .attr("dy", ".35em")
-        .text(function(d) { return "$ " + d.average_salary.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'); });
-
-    chart.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0," + (height - 70) + ")")
-      .call(xAxis);
-
-    chart.append("g")
-     .attr("class", "axis")
-     .call(yAxis);
+    rects = svg.selectAll('rect')
+               .data(dataSet)
+               .enter()
+               .append("rect")
+               .attr("x", 0)
+               .attr("y", function (d, i) {
+                 return h / dataSet.length * i
+               })
+               .attr("width", function (d) {
+                 return xScale(d.average)
+               })
+               .attr("height", function () {
+                 return h/dataSet.length - rectMargin
+               })
   }
+
+
 });
