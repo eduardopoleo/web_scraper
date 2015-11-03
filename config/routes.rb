fe65ed3738dc3f03218f955dd56eb6cb2f182edc
eduardopoleo <eduardopoleo@gmail.com>
2015-11-03 15:39:01 -0500

    Fixed layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'staff#horizontal'
+  root 'staff#vertical'
 
   get 'horizontal' => 'staff#horizontal'
   get 'vertical' => 'staff#vertical'
 
   # Example of regular route:
-  get 'all_salaries' => 'averages#all_salaries', :defaults => { :format => 'json' }
-  get 'professors_only' => 'averages#professors_only', :defaults => { :format => 'json' }
-  get 'administrative_staff' => 'averages#administrative_staff', :defaults => { :format => 'json' }
+  get 'all_salaries' => 'averages#all_salaries', defaults: { format:  'json' }
+  get 'professors_only' => 'averages#professors_only', defaults:  { format: 'json' }
+  get 'administrative_staff' => 'averages#administrative_staff', defaults: { format: 'json' }
 
   get 'practices(/:action)', controller: 'practices'
 
